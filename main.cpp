c18b94e171c5f81fc4c3be9bd75e254db6fd017d
ken0592 <kenken29862986@keio.jp>
2026-04-03 05:06:58 +0000

    a

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+double freq_A = 7519429 / (7519429 * 2 + 4637676 * 2);
+int freq_C = 4637676 / (7519429 * 2 + 4637676 * 2);
+int freq_G = 4637676 / (7519429 * 2 + 4637676 * 2);
+int freq_T = 7519429 / (7519429 * 2 + 4637676 * 2);
+
+
+int main() {
+    cout << freq_A << endl;
+}
