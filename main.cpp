51f248dfdce2cbadd691bd20cae1b8631fb81d9f
ken0592 <kenken29862986@keio.jp>
2026-04-06 15:39:04 +0000

    aaa

--- main.cpp
+++ main.cpp
@@ -13,6 +13,7 @@ vector <double> q = {freq_A, freq_C, freq_G, freq_T};
 
 vector <string> motif_names = {"MATa1", "MATalpha2", "MCM1", "MIG1", "PHO4", "RCS1", "ROX1", "TAF"};
 
+//モチーフじゃなくて転写因子にした方がよかった
 struct motif {
     string motif_name;
     int motif_size;
@@ -27,16 +28,22 @@ struct motif {
     }
 };
 
-vector <motif> motifs;
+struct fasta {
+    string name;
+    string seq;
+};
 
 int main() {
+    //各転写因子の対数オッズスコアの計算
+    vector <motif> motifs;
+
     for(int i = 0; i < motif_names.size(); i++){
         ifstream file("data/motif/" + motif_names.at(i));
 
         string seq;
         int len_seq;
         int num_seq = 0;
-
+        //各コンセンサス配列についてwhile
         while (getline(file, seq)) {
             if(num_seq == 0){
                 len_seq = seq.size();
@@ -57,13 +64,31 @@ int main() {
                 motifs.at(i).s.at(j).at(k) = log(motifs.at(i).p.at(j).at(k) / q.at(j));
             }
         }
+        //対数オッズスコア計算完了
         file.close();
     }
 
-    for(int j = 0; j < 4; j++){
-        for(int k = 0; k < 10; k++){
-            cout << motifs.at(0).s.at(j).at(k) << " ";
+    //各プロモータ配列の取得
+    vector <fasta> promoter;
+    string line;
+    fasta current;
+
+    ifstream file ("data/seq/promoters");
+
+    while(getline(file,line)){
+        if(line[0] == '>'){
+            if(!current.name.empty()) promoter.push_back(current);
+            current.name = line.substr(1);
+            current.seq = "";
         }
-        cout << endl;
+        else current.seq += line;
+    }
+    promoter.push_back(current);
+    file.close();
+    //各プロモータ配列の取得終了
+    
+    //各モチーフについて全プロモータ配列を回す
+    for(int i = 0; i < motifs.size(); i++){
+        cout << "Motif:" << motifs.at(i).motif_name << endl << endl;
     }
 }
