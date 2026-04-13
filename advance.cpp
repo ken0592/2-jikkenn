11b423dd79822207aac14c041b91013f84870d0c
ken0592 <kenken29862986@keio.jp>
2026-04-13 10:23:57 +0000

    cccc

--- advance.cpp
+++ advance.cpp
@@ -21,10 +21,8 @@ struct promoter{
 
 int L = 7;
 
-vector <vector<double>> p(4, vector<double>(L,1));
-vector <vector<double>> s(4, vector<double>(L,0));
-
-
+vector <vector<double>> p(4, vector<double>(L,1.0));
+vector <vector<double>> s(4, vector<double>(L,0.0));
 
 int main(){
     vector <promoter> promoters;
@@ -50,39 +48,60 @@ int main(){
         uniform_int_distribution<int> dist(0,promoters.at(i).seq.size()-L);
         promoters.at(i).pos = dist(gen);
     }
+    int count = 0;
+    while(true){
+        count++;
+        uniform_int_distribution<int> dist(0,promoters.size()-1);
+        int N = dist(gen); 
 
-    uniform_int_distribution<int> dist(0,promoters.size()-1);
-    int N = dist(gen); 
-
-    for(int i = 0; i < promoters.size(); i++){
-        if(i == N) continue;
-        for(int j = 0; j < L; j++){
-            if(promoters.at(i).seq.at(promoters.at(i).pos+j) == 'A') p.at(0).at(j)++;
-            else if(promoters.at(i).seq.at(promoters.at(i).pos+j) == 'C') p.at(1).at(j)++;
-            else if(promoters.at(i).seq.at(promoters.at(i).pos+j) == 'G') p.at(2).at(j)++;
-            else if(promoters.at(i).seq.at(promoters.at(i).pos+j) == 'T') p.at(3).at(j)++;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < L; j++){
+                p.at(i).at(j) = 1.0;
+            }
         }
-    }
 
-    for(int i = 0; i < 4; i++){
-        for(auto& k : p.at(i)){
-            k /= (promoters.size()-1+4);
+        for(int i = 0; i < promoters.size(); i++){
+            if(i == N) continue;
+            for(int j = 0; j < L; j++){
+                if(promoters.at(i).seq.at(promoters.at(i).pos+j) == 'A') p.at(0).at(j)++;
+                else if(promoters.at(i).seq.at(promoters.at(i).pos+j) == 'C') p.at(1).at(j)++;
+                else if(promoters.at(i).seq.at(promoters.at(i).pos+j) == 'G') p.at(2).at(j)++;
+                else if(promoters.at(i).seq.at(promoters.at(i).pos+j) == 'T') p.at(3).at(j)++;
+            }
         }
-    }
 
-    double s_dif = 0;
-    for(int i = 0; i < 4; i++){
-        for(int j = 0; j < L; j++){
-            s_dif += abs(s.at(i).at(j) - log(p.at(i).at(j)/q.at(i)););
-            s.at(i).at(j) = log(p.at(i).at(j)/q.at(i));
+        for(int i = 0; i < 4; i++){
+            for(double& k : p.at(i)){
+                k /= (promoters.size()-1+4);
+            }
         }
-    }
-    
-    vector<double> scores;
-    for(int i = 0; i < L; i++){
-        if(promoters.at(N).seq.at())
+
+        double s_dif = 0;
+        for(int i = 0; i < 4; i++){
+            for(int j = 0; j < L; j++){
+                s_dif += abs(s.at(i).at(j) - log(p.at(i).at(j)/q.at(i)));
+                s.at(i).at(j) = log(p.at(i).at(j)/q.at(i));
+            }
+        }
+        if(s_dif < 0.000001) break;
+        
+        vector <double> scores(promoters.at(N).seq.size()-L+1,0.0);
+        for(int i = 0; i < promoters.at(N).seq.size()-L+1; i++){
+            for(int j = 0; j < L; j++){
+                if(promoters.at(N).seq.at(i+j) == 'A') scores.at(i) += s.at(0).at(j);
+                else if(promoters.at(N).seq.at(i+j) == 'C') scores.at(i) += s.at(1).at(j);
+                else if(promoters.at(N).seq.at(i+j) == 'G') scores.at(i) += s.at(2).at(j);
+                else if(promoters.at(N).seq.at(i+j) == 'T') scores.at(i) += s.at(3).at(j);
+            }
+        }
+        for(auto &k: scores){
+            k = exp(k);
+        }
+        discrete_distribution<int> dist2(scores.begin(), scores.end());
+        promoters.at(N).pos = dist2(gen);
     }
 
+    cout << count << endl;
 
     ofstream fout("adv_output.txt");
     /*for(int i = 0; i < promoters.size(); i++){
@@ -91,11 +110,32 @@ int main(){
         fout << promoters.at(i).pos << endl << endl;
     }*/
 
-    /*for(int i = 0; i < 4; i++){
+    fout << "--------対数オッズ表-------" << endl;
+    for(int i = 0; i < 4; i++){
         for(double& k : s.at(i)){
             fout << k << " ";
         }
         fout << endl;
-    }*/
+    }
+    fout << "--------------------------" << endl;
+
+    double score;
+    for(int i = 0; i < promoters.size(); i++){
+        fout << "promoter:" << promoters.at(i).name << endl << endl;
+        score = 0.0;
+        for(int j = 0; j < promoters.at(i).seq.size()-L+1; j++){
+            for(int k = 0; k < L; k++){
+                if(promoters.at(i).seq.at(j+k) == 'A') score += s.at(0).at(k);
+                else if(promoters.at(i).seq.at(j+k) == 'C') score += s.at(1).at(k);
+                else if(promoters.at(i).seq.at(j+k) == 'G') score += s.at(2).at(k);
+                else if(promoters.at(i).seq.at(j+k) == 'T') score += s.at(3).at(k);
+            }
+            if(score > 2.0){
+                fout << "Pos:" << j+1 << "~" << j+L << endl;
+                fout << "hit(" << promoters.at(i).seq.substr(j,L) << ") = " << score << endl;
+            }
+        }
+        fout << "--------------------------" << endl;
+    }
     fout.close();
 }
